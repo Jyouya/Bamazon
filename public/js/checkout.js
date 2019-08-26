4accf14d2c20f6fdda7ec282ecdbd571460e4106
Will West <willwest8xp@gmail.com>
2019-08-26 14:28:43 -0400

    end of day commit

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,11 +1,17 @@
-const cart = localStorage.getItem('cart');
+const cart = JSON.parse(localStorage.getItem('cart'));
+let items;
 
 if (!cart) {
-    
+    $('#items').append($('<h1 class="title is-1">').text('Cart is empty'));
+} else {
+    items = new Promise().all(cart.map(item => {
+        $.get('/api/products/' + item.id)
+    }));
 }
 
-const items = new Promise.all()
 
 $(window).ready(function() {
-
+    if (items) {
+        (await items).forEach(appendItem);
+    }
 });
